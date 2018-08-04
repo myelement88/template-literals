aa82317c36085b386bfaa7daf589421aa614393a
Georg <myelement88@gmail.com>
2018-08-04 14:41:02 +0200

    Creates Repo

---
+++ main.js
@@ -0,0 +1,60 @@
+const petsData = [
+  {
+    name: "Purrsloud",
+    species: "Cat",
+    favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+    birthYear: 2017,
+    photo: "img/cat-2.jpg"
+  },
+  {
+    name: "Barksalot",
+    species: "Dog",
+    birthYear: 2008,
+    photo: "img/dog-1.jpg"
+  },
+  {
+    name: "Meowsalot",
+    species: "Cat",
+    favFoods: ["tuna", "catnip", "celery"],
+    birthYear: 2012,
+    photo: "img/cat-1.jpg"
+  }
+];
+
+function age(birthYear) {
+  let calcucatedAge = new Date().getFullYear() - birthYear;
+  if (calcucatedAge == 1) {
+    return "1 year old"
+  } else if (calcucatedAge == 0) {
+    return "Baby"
+  } else {
+    return `${calcucatedAge} years old`
+  }
+}
+
+function foods() {
+  return `
+    <h4>Favorite Foods</h4>
+    <ul class="foods-list">
+      <li>food1</li>
+      <li>food2</li>
+    </ul>
+  `
+}
+
+function petTemplate(pet) {
+  return `
+    <div class="animal">
+      <img class="pet-photo" src="${pet.photo}">
+      <h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
+      <p><strong>Age: </strong> ${age(pet.birthYear)}</p>
+      ${pet.favFoods ? foods() : ""}
+    </div>
+    `;
+}
+
+document.getElementById("app").innerHTML = `
+  <h1 class="app-title">Pets (${petsData.length} results)</h1>
+  ${petsData.map(petTemplate).join("")}
+  <p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates.
+`;
